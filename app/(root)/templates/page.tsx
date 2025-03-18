49e22b3405e17d02581dc5f78e5a0c2a9deff025
Ahmed Wagih <113104074+AhmeWagih@users.noreply.github.com>
2025-03-18 12:47:50 +0200

    Update diagram link path in page template from community to templates for improved navigation consistency.

--- app/(root)/templates/page.tsx
+++ app/(root)/templates/page.tsx
@@ -91,7 +91,7 @@ const Page = () => {
               <div className="p-4 flex flex-col gap-2">
                 <Link
                   className="hover:underline"
-                  href={`/community/${diagram.diagramID}`}
+                  href={`/templates/${diagram.diagramID}`}
                 >
                   <h3 className="font-medium text-lg">{diagram.title}</h3>
                 </Link>
